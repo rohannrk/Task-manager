dfe82924d7d5375148e76ba92f83901662e2ad55
Rohan <rohanrk39357@gmail.com>
2023-08-19 18:24:15 +0530

    Done

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const connectDB = require('./server/db/connect')
 const express = require('express');
 const app = express();
-const tasks = require('./server/routes/tasks')
-require('dotenv').config()
+const tasks = require('./server/routes/tasks');
+const connectDB = require('./server/db/connect');
+require('dotenv').config();
+const notFound = require('./server/middleware/not-found');
+const errorHandlerMiddleware = require('./server/middleware/error-handler');
 
-//middleware
-app.use(express.static('./public'))
-app.use(express.json())
+// middleware
 
+app.use(express.static('./public'));
+app.use(express.json());
 
-app.use('/api/v1/tasks', tasks)
+// routes
 
-const port = 3000;
+app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 3000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening on port ${port}...`))
-    } catch(error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
